71c7f317b7c41e2659826bfbe3d0ec09eaa6c0cd
Cihan <cihannakgul@gmail.com>
2023-06-19 22:47:27 +0300

    artist cell model

---
+++ UIComponents/UIComponents/Cell/CategoriesCell/CategoriesCellModel.swift
@@ -0,0 +1,36 @@
+//
+//  CategoriesCellModel.swift
+//  UIComponents
+//
+//  Created by cihan on 17.05.23.
+//
+import Foundation
+
+public protocol CategoriesCellDataSource: AnyObject {
+    var categoriesImageView: String? { get set }
+    var categoriesTitle: String? { get set }
+    var categoriesId: Int? { get set }
+}
+
+public protocol CategoriesCellEventSource: AnyObject {
+    
+}
+
+public protocol CategoriesCellProtocol: CategoriesCellDataSource, CategoriesCellEventSource {
+    
+}
+
+public final class CategoriesCellModel: CategoriesCellProtocol {
+    public var categoriesId: Int?
+    public var categoriesTitle: String?
+    public var categoriesImageView: String?
+
+
+    
+    public init(categoriesId: Int?, categoriesTitle: String?, categoriesImageView: String? ) {
+        self.categoriesId = categoriesId
+        self.categoriesTitle = categoriesTitle
+        self.categoriesImageView = categoriesImageView
+
+    }
+}
